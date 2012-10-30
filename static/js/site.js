813b554dffecf80f090426a1fa807a86f97376a7
James Long <jlongster@jlongster.com>
2012-10-30 15:10:36 -0400

    convert signin to use nav.get instead of nav.watch

--- static/js/site.js
+++ static/js/site.js
@@ -1,63 +1,53 @@
 
-// navigator.id.watch({
-//     loggedInUser: window.CURRENT_USER || null,
+function personaLogin() {
+    navigator.id.get(function(assertion) {
+        if(!assertion) {
+            return;
+        }
 
-//     onlogin: function(assertion) {
-//         if(!assertion) {
-//             return;
-//         }
+        $.ajax({
+            type: 'POST',
+            url: '/persona/verify',
+            data: JSON.stringify({
+                assertion: assertion
+            }),
+            processData: false,
+            contentType: 'application/json',
+            success: function(data) {
+                try {
+                    data = JSON.parse(data);
 
-//         $.ajax({
-//             type: 'POST',
-//             url: '/persona/verify',
-//             data: JSON.stringify({
-//                 assertion: assertion
-//             }),
-//             processData: false,
-//             contentType: 'application/json',
-//             success: function(data) {
-//                 try {
-//                     data = JSON.parse(data);
+                    if (data.status === "okay") {
+                        if(data.freshman) {
+                            // If new user, show introduction page
+                            window.location = '/freshman';
+                        }
+                        else {
+                            window.location = location;
+                        }
 
-//                     if (data.status === "okay") {
-//                         // The persona server seems to need a little
-//                         // time to log in the user, otherwise the
-//                         // `nav.id.watch` is kinda flaky
-//                         setTimeout(function() {
-//                             if(data.freshman) {
-//                                 // If new user, show introduction page
-//                                 window.location = '/freshman';
-//                             }
-//                             else {
-//                                 window.location = location;
-//                             }
-//                         }, 1500);
-
-//                         $('.login').html('Logging in... <img src="/img/loader.gif" />');
-//                     } else {
-//                         alert('Login failed, try again later');
-//                     }
-//                 } catch (ex) {
-//                     // oh no, we didn't get valid JSON from the server
-//                 }
-//             }
-//         });
-//     },
-
-//     onlogout: function() {
-//         $.post('/persona/logout', function() {
-//             window.location = '/';
-//         });
-//     }
-// });
+                        $('.login').html('Logging in... <img src="/img/loader.gif" />');
+                    } else {
+                        alert('Login failed, try again later');
+                    }
+                } catch (ex) {
+                    // oh no, we didn't get valid JSON from the server
+                }
+            }
+        });
+    });
+}
 
 $("#browserid").click(function(){
-    navigator.id.request();
+    personaLogin();
 });
 
 $('.login a.logout').click(function(e) {
     e.preventDefault();
-    navigator.id.logout();
+
+    $.post('/persona/logout', function() {
+        window.location = '/';
+    });
 });
 
 $('button.next, a.next').live('click', function(e) {
